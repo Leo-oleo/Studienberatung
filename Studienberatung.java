9fbd4149c0cbda3ec5e23554aa61edd5e2b76ff9
Leo-oleo <leo-oleo@hotmail.de>
2017-10-29 12:48:24 +0100

    Klasse scanner wurde initialisiert

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Studienberatung{ 
 	public static void main(String[] arg){
 		
+		Scanner tastatur = new Scanner(System.in);
+		String antwort = tastatur.next();
 			
 		
 	}
