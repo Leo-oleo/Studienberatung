2a287abb7dd85925d59badf7b69c689d67c14246
Leo-oleo <leo-oleo@hotmail.de>
2017-10-29 13:30:55 +0100

    hast-du-fachabitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -10,16 +10,7 @@ public class Studienberatung{
 		else {
 			System.out.println("Hast du Fachabitur?");
 			antwort = tastatur.next();
-			if (antwort.equals("ja")){
-				System.out.println("Hast du eine Berufsausbildung?");
-				antwort = tastatur.next();
-					if (antwort.equals("ja")){
-						System.out.println("Du kannst ja später noch studieren");
-					}
-					else {
-						System.out.println("Besser noch studieren");
-					}
-			}
+			
 			else {
 				System.out.println("Nicht studieren");
 			}
