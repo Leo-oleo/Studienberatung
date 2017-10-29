5a3b16039c77e91299b3cc6fae808f25deb37344
Leo-oleo <leo-oleo@hotmail.de>
2017-10-29 12:54:17 +0100

    Frage: Hast du Abitur? wurde hinzugefügt und die Antwort wird rausgelesen

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Studienberatung{ 
 	public static void main(String[] arg){
-		
+		System.out.println("Hast du Abitur?");
 		Scanner tastatur = new Scanner(System.in);
 		String antwort = tastatur.next();
 			
